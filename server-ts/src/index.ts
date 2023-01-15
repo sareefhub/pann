fcd80464133b6f13a6dc46c188c2411ba540f8f7
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2023-01-15 09:42:45 +0700

    เพิ่ม config ใน index/src และแก้ไข port เป็น 8000

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -2,10 +2,13 @@ import Koa from 'koa'
 import json from 'koa-json'
 import apiRouter from './api'
 import loadFixtures from './fixtures'
+import appConfig from './config'
 
 const app = new Koa()
 
 app.use(json())
 app.use(apiRouter.routes())
 
-app.listen(3306) 
+app.listen(8000)
+
+loadFixtures(appConfig.ClearDataBeforeLoad)
