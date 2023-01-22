fa0381457df67419581706a25d10013994ac3420
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2023-01-23 00:31:38 +0700

    ∞สร้าง pann-app-bar ใน components

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,41 +1,16 @@
-import Repo from "./repositories"
-import { useEffect,useState } from "react";
-import Announcement from "./models/Announcement";
-
+import './App.css';
+import AppRoutes from './config/routes';
+import { AuthProvider } from "react-oidc-context";
+import { oidcConfig } from './config';
+import AppProvider from './AppProvider';
 
 function App() {
-  const [announcementL, setAnnouncementL] = useState<Announcement[]>([])
-
-  const fetchannouncementL = async () => {
-    const result = await Repo.announcements.getAll()
-    if(result){
-      setAnnouncementL(result)
-    }
-  }
-
-  useEffect(() => {
-    fetchannouncementL()
-  })
-  return(
-    <div>
-      {announcementL.map(announcement => (
-        <div key={announcement.id}>
-          <p>ID : {announcement.id}</p>
-          <p>Description : {announcement.description}</p>
-          <p>Positive : {announcement.remarkIfPositive}</p>
-          <p>Negative : {announcement.remarkIfNegative}</p>
-          <p>Time : {announcement.pubDateTime.toLocaleString('en-US', {
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit',
-              second: '2-digit'
-            })}</p>
-          <p>UserCode : {announcement.userCode}</p>
-        </div>
-      ))}
-    </div>
+  return (
+    <AuthProvider {...oidcConfig}>
+      <AppProvider>
+        <AppRoutes />
+      </AppProvider>      
+    </AuthProvider>
   )
 }
 
