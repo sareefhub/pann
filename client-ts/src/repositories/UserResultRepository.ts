0e5cf3681122cfa34895577507c0f4deeaf4eb33
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2023-01-23 00:21:13 +0700

    ∞แก้ไข UserResult

--- client-ts/src/repositories/UserResultRepository.ts
+++ client-ts/src/repositories/UserResultRepository.ts
@@ -1,40 +1,60 @@
-import UserResult from "../models/UserResult";
+import { ax } from '../config';
 import { IRepository } from "./IRepository";
+import UserResult from "../models/UserResult";
+import config from "../config";
 
-export class userResultRepo implements IRepository<UserResult> {
-    get(id: string | number): Promise<UserResult | null> {
-        throw new Error("Method not implemented.");
-    }
-    create(entity: Partial<UserResult>): Promise<UserResult | null> {
-        throw new Error("Method not implemented.");
-    }
-    update(entity: Partial<UserResult>): Promise<UserResult | null> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string | number): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    async getAll(filter: any): Promise<UserResult[] | null> {
-        return [
-            {id: 1,
-            announcement: {
-                id: 1, 
-                topic: '240-124 Midterm 1/2566',
-                description: 'คะแนนกลางภาควิชา Web Dev',
-                remarkIfPositive:'1',
-                remarkIfNegative:'0',
-                pubDateTime:new Date('2022-09-08 10:00:00'), 
-                userCode: 'suthon.s'},
-            result: 'ไม่ได้รับทุน',
-            resultType: 1,
-            remark: '',
-            isPinned: false,
-            viewDateTime: new Date('2022-09-08 15:00:00'),
-            ackDateTime: new Date('2022-09-08 15:15:15'),
-            updateDateTime: new Date('2022-09-07 10:00:00'),
-            expireDateTime: new Date('2022-09-15 12:00:00'),
-            userCode: '6210110227'
-            }
-        ]
-    }
+export interface UserResultFilter {
+  keyword?: string
+  isPinned?: boolean
+}
+
+export class UserResultRepository implements IRepository<UserResult> {
+  urlPrefix = config.remoteRepositoryUrlPrefix
+
+  async getAll(filter: UserResultFilter): Promise<UserResult[] | null> {
+    const params = {...filter}
+    const resp = await ax.get<UserResult[]>(`${this.urlPrefix}/userResult`, { params })
+
+    return resp.data
+  }
+
+  async get(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/${id}`)
+
+    return resp.data
+  }
+
+  async create(entity: Partial<UserResult>): Promise<UserResult | null> {
+    const resp = await ax.post<UserResult>(`${this.urlPrefix}/userResult`, entity)
+
+    return resp.data
+  }
+
+  async update(entity: Partial<UserResult>): Promise<UserResult | null> {
+    const resp = await ax.put<UserResult>(`${this.urlPrefix}/userResult/${entity.id}`, entity)
+
+    return resp.data
+  }
+
+  async delete(id: string|number): Promise<void> {
+    await ax.delete<void>(`${this.urlPrefix}/userResult/${id}`)
+  }
+
+  async view(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/view/${id}`)
+
+    return resp.data
+  }
+
+  async acknowledge(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/acknowledge/${id}`)
+
+    return resp.data
+  }
+
+  async toggleIsPinned(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/toggleIsPinned/${id}`)
+
+    return resp.data
+  }
 }
