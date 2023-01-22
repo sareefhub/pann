7797fbd46a7bc0650edbff6272553f5a5b9e0933
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2023-01-23 01:28:35 +0700

    ∞แก้ขไฟล์

--- client-ts/src/models/Announcement.tsx
+++ client-ts/src/models/Announcement.tsx
@@ -1,9 +1,9 @@
 export default interface Announcement{
-    id: number
-    topic: string
-    description: string
-    remarkIfPositive: string
-    remarkIfNegative: string
-    pubDateTime: Date
-    userCode: string
-  }
+  id: number
+  topic: string
+  description: string
+  remarkIfPositive: string
+  remarkIfNegative: string
+  pubDateTime: Date
+  userCode: string
+}
